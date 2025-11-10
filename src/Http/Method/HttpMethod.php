af037a22cddf23e16eb2e15bcf23d4e940a96eb6
nihad1213 <nihad.nemetli@gmail.com>
2025-11-10 15:37:09 +0400

    new: HttpMethod enum created

---
+++ src/Http/Method/HttpMethod.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Http\Method;
+
+enum HttpMethod: string
+{
+    case GET = 'GET';
+    case POST = 'POST';
+    case PUT = 'PUT';
+    case DELETE = 'DELETE';
+    case PATCH = 'PATCH';
+    case HEAD = 'HEAD';
+    case OPTIONS = 'OPTIONS';
+}
